c9211ed76b721a096ff94f3529c0cb1f839f4add
Akhil <akhil@Akhils-MacBook-Pro.local>
2020-02-28 16:33:24 -0500

    added routing

--- easy-bev-app/src/index.js
+++ easy-bev-app/src/index.js
@@ -1,11 +1,33 @@
 import index from "./js/index";
 import Qrbutton from "./components/Qrbutton";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={Qrbutton} />
+            <Route path="/login" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+
+
+        </div>
+    </Router>
+)
+
+
+{/*class App extends React.Component {
     render() {
         return <Qrbutton></Qrbutton>
     }
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))*/}
+
+ReactDOM.render(
+    routing, document.getElementById('root')
+);
