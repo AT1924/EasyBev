dcd8fa58ec6f2b4794e61fab55bbf33e2487a3cb
Akhil <akhil@Akhils-MacBook-Pro.local>
2020-03-07 06:52:43 -0500

    added grid layout to footer

--- easy-bev-app/src/components/Footer.js
+++ easy-bev-app/src/components/Footer.js
@@ -5,11 +5,15 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import LibraryBooksOutlinedIcon from '@material-ui/icons/LibraryBooksOutlined'
 import AssignmentIcon from '@material-ui/icons/Assignment'
 import ChatIcon from '@material-ui/icons/Chat'
+import Grid from '@material-ui/core/Grid';
+
 
 class Footer extends React.Component {
     constructor(props){
         super(props);
         this.reader = null;
+
+
         this.state = {
             chosen : "FEED",
         }
@@ -17,19 +21,35 @@ class Footer extends React.Component {
     update(whereto){
         this.state = {chosen: whereto}
     }
+
     render() {
         return (<div id = "footer">
-            <IconButton onclick={this.update("FEED")} color = "primary"><LibraryBooksOutlinedIcon/></IconButton>
-            <IconButton onclick={this.update("CART")} color = "secondary"><ShoppingCartIcon/></IconButton>
-            <IconButton onclick={this.update("ORDERS")} color = "secondary"><AssignmentIcon/></IconButton>
-            <IconButton onclick={this.update("MESSAGE")} color = "secondary"><ChatIcon/></IconButton>
-        </div>)
+            <Grid container style={style.root} spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container direction="row" justify="center" alignItems="flex-end" spacing={2}>
+
+                        <IconButton onclick={this.update("FEED")} color = "primary"><LibraryBooksOutlinedIcon/></IconButton>
+                        <IconButton onclick={this.update("CART")} color = "secondary"><ShoppingCartIcon/></IconButton>
+                        <IconButton onclick={this.update("ORDERS")} color = "secondary"><AssignmentIcon/></IconButton>
+                        <IconButton onclick={this.update("MESSAGE")} color = "secondary"><ChatIcon/></IconButton>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+
+
+        </div>
+
+
+        )
     }
 
 }
 
 const style = {
-
+    root: {
+        flexGrow: 1,
+    }
 }
 
 export default Footer;
