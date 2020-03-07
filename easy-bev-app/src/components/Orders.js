069f037d6049d3748c3f0e088c8f3211f1bb9ccf
Akhil <akhil@Akhils-MacBook-Pro.local>
2020-03-07 18:07:54 -0500

    ordering template set up

--- easy-bev-app/src/components/Orders.js
+++ easy-bev-app/src/components/Orders.js
@@ -13,20 +13,11 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import OrderComponent from './OrderComponent'
+import OrderTitle from './OrderTitle';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Box from '@material-ui/core/Box';
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import Nav from "./Nav";
 
 const useStyles = makeStyles(theme => ({
     '@global': {
@@ -107,24 +98,6 @@ const tiers = [
         buttonVariant: 'outlined',
     },
 ];
-const footers = [
-    {
-        title: 'Company',
-        description: ['Team', 'History', 'Contact us', 'Locations'],
-    },
-    {
-        title: 'Features',
-        description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-    },
-    {
-        title: 'Resources',
-        description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-    },
-    {
-        title: 'Legal',
-        description: ['Privacy policy', 'Terms of use'],
-    },
-];
 
 export default function Pricing() {
     const classes = useStyles();
@@ -132,54 +105,25 @@ export default function Pricing() {
     return (
         <React.Fragment>
             <CssBaseline />
-            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-                <Toolbar className={classes.toolbar}>
-                    <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-                        Company name
-                    </Typography>
-                    <nav>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Features
-                        </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Enterprise
-                        </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Support
-                        </Link>
-                    </nav>
-                    <Button href="#" color="primary" variant="outlined" className={classes.link}>
-                        Login
-                    </Button>
-                </Toolbar>
-            </AppBar>
+            <Nav/>
 
-
-            {/* Footer */}
-            <Container maxWidth="md" component="footer" className={classes.footer}>
-                <Grid container spacing={4} justify="space-evenly">
-                    {footers.map(footer => (
-                        <Grid item xs={6} sm={3} key={footer.title}>
-                            <Typography variant="h6" color="textPrimary" gutterBottom>
-                                {footer.title}
-                            </Typography>
-                            <ul>
-                                {footer.description.map(item => (
-                                    <li key={item}>
-                                        <Link href="#" variant="subtitle1" color="textSecondary">
-                                            {item}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </Grid>
-                    ))}
+            <Container maxWidth="md" component="main">
+                <Grid container spacing={5} alignItems="flex-end">
+                    <Grid item>
+                        <OrderTitle/>
+                        <OrderComponent/>
+                    </Grid>
                 </Grid>
-                <Box mt={5}>
-                    <Copyright />
-                </Box>
             </Container>
-            {/* End footer */}
+
+            <Container maxWidth="md" component="footer" className={classes.footer}>
+            <Grid container spacing={4} justify="space-evenly">
+
+            </Grid>
+        </Container>
+
+
+
         </React.Fragment>
     );
 }
