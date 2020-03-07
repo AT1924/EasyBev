b52e26cfd8cb65760ab124bca0062da42651f3cb
Benjamin E Deckey <benjaminedeckey@Benjamins-MacBook-Pro.local>
2020-03-07 17:26:01 -0500

    added signin connection

--- easy-bev-app/src/components/SignIn.js
+++ easy-bev-app/src/components/SignIn.js
@@ -12,6 +12,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 function Copyright() {
     return (
@@ -26,7 +28,7 @@ function Copyright() {
     );
 }
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
     paper: {
         marginTop: theme.spacing(8),
         display: 'flex',
@@ -44,74 +46,120 @@ const useStyles = makeStyles(theme => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
-}));
+});
 
-export default function SignIn() {
-    const classes = useStyles();
+class SignIn extends React.Component {
+    state = {
+        email: '', password: '', error: false, errorMsg: '', redirect: false,
+    };
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign In
-                    </Button>
-                    <Grid container>
-                        {/*<Grid item xs>*/}
-                        {/*    <Link href="#" variant="body2">*/}
-                        {/*        Forgot password?*/}
-                        {/*    </Link>*/}
-                        {/*</Grid>*/}
-                        <Grid item>
-                            <Link href="/signup" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
+    signIn = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: this.state.email, password: this.state.password }),
+        });
+        const body = await response.json();
+        console.log(body);
+        if ("error" in body) {
+            console.log(body);
+            this.setState({ errorMsg: body.error});
+
+            return false;
+        }
+        else if ("first_name" in body) {
+            console.log("signed in");
+        }
+        else {
+            console.log(body);
+        }
+
+        return false;
+    };
+
+    handleChange = name => (event) => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} noValidate onSubmit={this.signIn}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={this.handleChange('email')}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={this.handleChange('password')}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            {/*<Grid item xs>*/}
+                            {/*    <Link href="#" variant="body2">*/}
+                            {/*        Forgot password?*/}
+                            {/*    </Link>*/}
+                            {/*</Grid>*/}
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-        </Container>
-    );
+                    </form>
+                </div>
+                <Box mt={8}>
+                    <Copyright />
+                </Box>
+            </Container>
+        );
+    }
+
+
 }
+
+SignIn.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SignIn);
